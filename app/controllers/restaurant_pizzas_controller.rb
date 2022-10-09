e635413faf0f53a5f90572df404acdc1632347bf
Okubasufrank <franklineokubasu51@gmail.com>
2022-10-09 16:15:32 +0300

    done

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+    def create 
+        restaurant = RestaurantPizza.create!(restaurant_pizza_params)
+        render json: restaurant.pizza, 
+    end
+
+
+
+    private
+    def restaurant_pizza_params
+        params.permit(:price, :restaurant_id, :pizza_id)
+    end
+
+    def unprocessable_entity
+        render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
